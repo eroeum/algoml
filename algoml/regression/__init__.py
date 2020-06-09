cafa4ad285206abed9d01d753dacad48e722f0c3
Eric Roeum <eroeum@gmail.com>
2020-06-08 22:51:51 -0400

    Adds logistic regression

--- algoml/regression/__init__.py
+++ algoml/regression/__init__.py
@@ -1,1 +1,2 @@
 from .linear_regression.linear_regression import LinearRegressionPipeline
+from .logistic_regression.logistic_regression import LogisticRegressionPipeline
--- run.py
+++ run.py
@@ -2,11 +2,16 @@ from algoml.classification import BinaryClassificationPipeline
 from algoml.classification import MulticlassClassificationPipeline
 from algoml.classification import MultilabelClassificationPipeline
 from algoml.regression import LinearRegressionPipeline
+from algoml.regression import LogisticRegressionPipeline
 from algoml.svm import LinearSVMPipeline
 from algoml.svm import NonlinearSVMPipeline
 
 def run_linear_regession_pipeline():
     r = LinearRegressionPipeline()
+    r.run()
+
+def run_logistic_regession_pipeline():
+    r = LogisticRegressionPipeline()
     r.run()
 
 def run_binary_classification_pipeline():
@@ -30,4 +35,4 @@ def run_nonlinear_svm_pipeline():
     s.run()
 
 if __name__ == '__main__':
-    run_nonlinear_svm_pipeline()
+    run_logistic_regession_pipeline()
